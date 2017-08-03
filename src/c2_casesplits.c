cea04e8b77296d3e9e63662d18c77aa4a4f85ae7
CameronRasmussen <crasmussen@berkeley.edu>
2017-08-02 17:16:39 -0700

    Minor bugfixes

--- src/c2_casesplits.c
+++ src/c2_casesplits.c
@@ -15,24 +15,24 @@
 
 void c2_backtrack_case_split(C2* c2) {
     V2("Backtracking from case split.\n");
-    
+
     V1("Finished case:");
     for (unsigned i = 0; i < int_vector_count(c2->case_split_stack); i++) {
         V1(" %d", int_vector_get(c2->case_split_stack, i));
     }
     V1("\n");
-    
+
     c2_backtrack_to_decision_lvl(c2, c2->restart_base_decision_lvl);
     V3("Now popping the case split.\n");
     stack_pop(c2->stack, c2);
     c2_pop(c2);
-    
+
     c2->skolem->decision_lvl -=1;
     c2->restart_base_decision_lvl -= 1;
-    
+
     assert(c2->skolem->decision_lvl == 0); // TODO: If we do case splits after dlvl 0, we should also add a real clause
     assert(c2->restart_base_decision_lvl == 0);
-    
+
     int_vector* solved_cube = int_vector_init();
     vector_add(c2->skolem->cegar->solved_cubes, solved_cube);
     for (unsigned i = 0; i < int_vector_count(c2->case_split_stack); i++) {
@@ -43,7 +43,7 @@ void c2_backtrack_case_split(C2* c2) {
         int_vector_add(solved_cube, -l);
     }
     satsolver_clause_finished(c2->skolem->skolem);
-    
+
     // check learnt clauses for unique consequences ... the last backtracking may have removed the unique consequences
     for (unsigned i = vector_count(c2->qcnf->clauses); i > 0; i--) {
         Clause* c = vector_get(c2->qcnf->clauses, i-1);
@@ -56,16 +56,16 @@ void c2_backtrack_case_split(C2* c2) {
             }
         }
     }
-    
+
     int_vector_reset(c2->case_split_stack);
-    
+
     // Test if we exhausted all cases
     assert(int_vector_count(c2->case_split_stack) == 0);
     if (satsolver_sat(c2->skolem->skolem) == SATSOLVER_UNSATISFIABLE) {
         V1("Universal assignments depleted: SAT\n");
         c2->result = CADET_RESULT_SAT;
     }
-    
+
 }
 
 void c2_case_split_backtracking_heuristics(C2* c2) {
@@ -76,24 +76,24 @@ void c2_case_split_backtracking_heuristics(C2* c2) {
 unsigned c2_assume_constant(C2* c2, Lit lit) {
     assert(!skolem_can_propagate(c2->skolem));
     statistics_start_timer(c2->statistics.failed_literals_stats);
-    
+
     size_t case_split_decision_metric = c2->skolem->statistics.propagations;
-    
+
     skolem_push(c2->skolem);
     skolem_assume_constant_value(c2->skolem, lit);
     skolem_propagate(c2->skolem);
-    
+
     if (skolem_is_conflicted(c2->skolem)) {
         V1("Skolem conflict with assumed constant %d: %d\n", lit, c2->skolem->conflict_var_id);
         c2->statistics.failed_literals_conflicts++;
         case_split_decision_metric += 1024; //Magic number to ensure the variable is chosen
     }
-    
+
     V2("Number of propagations when assigning %d: %zu\n", lit, c2->skolem->statistics.propagations - case_split_decision_metric);
-    
+
     skolem_pop(c2->skolem);
     statistics_stop_and_record_timer(c2->statistics.failed_literals_stats);
-    
+
     return case_split_decision_metric;
 }
 
@@ -115,9 +115,9 @@ Lit c2_case_split_pick_literal(C2* c2) {
 }
 
 bool c2_case_split(C2* c2) {
-    
+
     bool progress = false; // indicates whether this function call changed anything.
-    
+
 //    Lit most_notorious_literal = c2_pick_most_notorious_literal(c2);
     Lit most_notorious_literal = c2_case_split_pick_literal(c2);
     if (most_notorious_literal != 0) {
@@ -125,17 +125,17 @@ bool c2_case_split(C2* c2) {
         if (debug_verbosity >= VERBOSITY_LOW) {
             V1("Found notorious literal");
             options_print_literal_name(c2->options, c2_literal_color(c2, NULL, most_notorious_literal), most_notorious_literal);
-            V1(" with notoriousity %.2f; greater than threshold %.2f.\n", notoriousity, threshold);
+            V1(" \n");
         }
-        
+
         satsolver_assume(c2->skolem->skolem, skolem_get_satsolver_lit(c2->skolem, most_notorious_literal));
-        
+
         sat_res res = satsolver_sat(c2->skolem->skolem);
         if (res != SATSOLVER_SATISFIABLE) {
             V1("This case admits no assignments to the universals that are consistent with dlvl 0, switching polarity and assuming %d instead.\n", - most_notorious_literal);
-            
+
             most_notorious_literal = - most_notorious_literal;
-            
+
             satsolver_assume(c2->skolem->skolem, skolem_get_satsolver_lit(c2->skolem, most_notorious_literal));
             sat_res res = satsolver_sat(c2->skolem->skolem);
             assert(c2->skolem->decision_lvl == c2->restart_base_decision_lvl);
@@ -154,7 +154,7 @@ bool c2_case_split(C2* c2) {
                 most_notorious_literal = 0; // suppresses that case split happens
             }
         }
-        
+
         if (most_notorious_literal != 0) {
             progress = true;
             if (int_vector_count(c2->case_split_stack) == 0) {
@@ -164,14 +164,14 @@ bool c2_case_split(C2* c2) {
                 c2->skolem->decision_lvl +=1;
                 c2->restart_base_decision_lvl += 1;
             }
-            
+
             int_vector_add(c2->case_split_stack, most_notorious_literal);
-            
+
             assert(skolem_is_deterministic(c2->skolem, lit_to_var(most_notorious_literal)));
             skolem_assume_constant_value(c2->skolem, most_notorious_literal);
-            
+
             skolem_propagate(c2->skolem);
-            
+
             if (skolem_is_conflicted(c2->skolem)) { // actual conflict
                 assert(c2->skolem->decision_lvl == c2->restart_base_decision_lvl); // otherwise we need to go into conflict analysis
                 V1("Case split lead to immediate conflict.\n");
@@ -232,13 +232,13 @@ int c2_pick_most_notorious_literal(C2* c2) {
         //        if (skolem_compare_dependencies(c2->skolem, skolem_get_dependencies(c2->skolem, i), c2->skolem->empty_dependencies) != DEPS_EQUAL) {
         //            continue;
         //        }
-        
+
         float notoriousity_pos = c2_notoriousity(c2,   (int) i);
         if (notoriousity_pos > notoriousity) {
             notoriousity = notoriousity_pos;
             notorious_lit = (Lit) i;
         }
-        
+
         float notoriousity_neg = c2_notoriousity(c2, - (int) i);
         if (notoriousity_neg > notoriousity) {
             notoriousity = notoriousity_neg;
@@ -249,19 +249,19 @@ int c2_pick_most_notorious_literal(C2* c2) {
 }
 
 int_vector* c2_determine_notorious_determinsitic_variables(C2* c2) {
-    
+
     float notoriousity_threshold = c2_notoriousity_threshold(c2);
     int_vector* notorious_lits = int_vector_init();
-    
+
     for (unsigned i = 1; i < var_vector_count(c2->qcnf->vars); i++) {
         if (! skolem_is_deterministic(c2->skolem, i) || skolem_lit_satisfied(c2->skolem, (int) i) || skolem_lit_satisfied(c2->skolem, - (int) i)) {
             continue;
         }
-        
+
         float notoriousity_pos = c2_notoriousity(c2,   (int) i);
         float notoriousity_neg = c2_notoriousity(c2, - (int) i);
         int more_notorious_val = notoriousity_pos > notoriousity_neg ? 1 : -1; // cannot be notorious in both polarities
-        
+
         if ( (more_notorious_val > 0 && notoriousity_pos > notoriousity_threshold) || (more_notorious_val < 0 && notoriousity_neg > notoriousity_threshold) ) {
             V1("  Identified %d as notorious (%.3f).\n", - more_notorious_val * (int) i, notoriousity_pos > notoriousity_neg ? notoriousity_pos : notoriousity_neg );
             int_vector_add(notorious_lits, - more_notorious_val * (int) i);

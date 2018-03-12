bd428b60c250cf32425eaf4651400e0192bb5061
Markus N Rabe <markus.norman.rabe@gmail.com>
2018-03-12 14:26:36 -0700

    minimizing originals less often

--- src/c2_simplify.c
+++ src/c2_simplify.c
@@ -37,11 +37,14 @@ void c2_delete_learnt_clauses_greater_than(C2* c2, unsigned max_size) {
 
 void c2_simplify(C2* c2) {
     assert(c2->restart_base_decision_lvl == c2->skolem->decision_lvl); // because conflicts we may find are treated as UNSAT
-    
+    bool simplify_originals = genrand_int32() % 10 ? false : true;
     for (unsigned i = 0; i < vector_count(c2->qcnf->active_clauses); i++) {
         if (c2->state != C2_READY) {break;}
         Clause* c = vector_get(c2->qcnf->active_clauses, i);
         if (! c->active || skolem_get_unique_consequence(c2->skolem, c) != 0) {
+            continue;
+        }
+        if (c->original && ! simplify_originals) {
             continue;
         }
         

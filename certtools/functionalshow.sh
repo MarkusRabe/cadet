f3a51e22f0966e410e80ac3aec22a5a10c9bd331
Markus N Rabe <markus.norman.rabe@gmail.com>
2018-04-16 11:56:39 -0700

    added missing certtools scripts

---
+++ certtools/functionalshow.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+./../cadet --sat_by_qbf -f tmp.aag $1
+
+aigtoaig tmp.aag tmp.aig
+# abc -c "read tmp.aig; write tmp.aig"
+abc -c "read tmp.aig; print_stats; dc2; dc2; dc2; print_stats; write tmp.aig"
+aigtoaig tmp.aig tmp.aag
+
+
+echo "Calling dot to generate the PDF"
+aigtodot tmp.aag | dot -Tpdf -o$1.pdf
+
+open $1.pdf
+
+rm tmp.aag tmp.aig $1.pdf
+
